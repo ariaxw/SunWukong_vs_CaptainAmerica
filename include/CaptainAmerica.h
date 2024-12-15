97dc899fb6d14613bfe1c7ce96584a0b904793b4
Aria <103609056+ariaxw@users.noreply.github.com>
2024-12-14 22:38:40 -0500

    Update CaptainAmerica.h

--- include/CaptainAmerica.h
+++ include/CaptainAmerica.h
@@ -2,13 +2,22 @@
 #include "Character.h"
 #include <SFML/Graphics.hpp>
 
+// Represents Captain America, derived from Character
 class CaptainAmerica : public Character {
 public:
-    CaptainAmerica() : Character("Captain America", 140, 10, 5) {}
+    // Constructor initializes Captain America with specific stats
+    CaptainAmerica() : Character("Captain America", 140, 12, 5) {}
     
+    // Override attack method
     void attack(Character& target) override;
+    
+    // Override defend method
     void defend() override;
+    
+    // Override special method
     void special(Character& target) override;
+    
+    // Override draw method to render Captain America
     void draw(sf::RenderWindow& window, sf::Vector2f position) override;  // Add draw method declaration
 };
 
